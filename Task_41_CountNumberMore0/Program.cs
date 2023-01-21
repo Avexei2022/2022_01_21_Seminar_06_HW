45a0994ccd99f31594a15c187e7007cde02ed17e
alex123 <alex123@dot.com>
2023-01-21 22:00:42 +0500

    The goal has been achieved

---
+++ Task_41_CountNumberMore0/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+PrintTask();
+int[] myArray = CreateArray();
+FillArray(myArray);
+int result = GetResult(myArray);
+PrintResult(myArray, result);
+
+void PrintTask() {
+    Console.WriteLine("Пользователь вводит с клавиатуры M чисел.\n" +
+    "Программа посчитает, сколько чисел больше 0 ввёл пользователь.\n");
+}
+
+int[] CreateArray() {
+    int arraySize = 0;
+    while (arraySize < 1) {
+        Console.Write("Определите кличество целых чисел (М): ");
+        int.TryParse(Console.ReadLine(), out arraySize);        
+    }
+    int[] array = new int[arraySize];
+    for(int i = 0; i < arraySize; i++) array[i] = 0;
+    return array;
+}
+
+int[] FillArray(int[] array) {
+    int value = 0;
+    Console.WriteLine();
+    for(int i = 0; i < array.Length; i++)  {
+        Console.Write($"Введите {i+1}-е целое число: ");
+        int.TryParse(Console.ReadLine(), out value);
+        array[i] = value;       
+    }
+    return array;
+}
+
+int GetResult(int[] array) {
+    int count = 0;
+    foreach (var item in array) {
+       if (item > 0) count++; 
+    }
+    return count;
+}
+
+void PrintResult (int[] array, int count) {
+    Console.WriteLine();
+    Console.Write("Вы ввели числа: ");
+    foreach (int item in array) Console.Write($"{item} ");
+    Console.WriteLine("\n");
+    Console.WriteLine($"Количество чисел больше 0 -> {count}\n");
+}
---
+++ Task_43_FindCrossVectorPoint/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+PrdoubleTask();
+double b1 = InputValue("b1");
+double k1 = InputValue("k1");
+double b2 = InputValue("b2");
+double k2 = InputValue("k2");
+double x = GetX(b1, k1, b2, k2);
+double y = GetY(b1, k1, x);
+PrdoubleResult(b1, k1, b2, k2, x, y);
+
+void PrdoubleTask() {
+    Console.WriteLine("Программа находит точку пересечения двух прямых,\n" +
+    "заданных уравнениями y = k1 * x + b1, y = k2 * x + b2.\n"+
+    "Значения b1, k1, b2 и k2 задаются пользователем.\n");
+}
+
+double InputValue(string point) {
+        Console.Write($"Введите значение {point} : ");
+        double.TryParse(Console.ReadLine(), out double value);
+        return value;    
+}
+
+double GetX(double b1, double k1, double b2, double k2) {
+    double x = (b2 - b1)/(k1 - k2);
+    return x;
+}
+
+double GetY(double b1, double k1, double x) {
+    double y = k1 * x + b1;
+    return y;
+}
+
+void PrdoubleResult(double b1, double k1, double b2, double k2, double x, double y) {
+    Console.WriteLine();
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x:N1}; {y:N1})\n");
+}
